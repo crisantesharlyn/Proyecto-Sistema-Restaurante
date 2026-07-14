a4200213fcd0b072af78ffca3ad9765ce1191c37
crisantesharlyn <sharlyn.crisantem@istpargentina.edu.pe>
2026-07-14 12:23:51 -0500

    se creo las librerias en el archivo models.py

---
+++ modelos/models.py
@@ -0,0 +1,5 @@
+#models.py - Sistema de Restaurante
+from sqlalchemy import Colum, Integrer, String, Numeric, ForeingKey, TIMESTAMP
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql import func
+
